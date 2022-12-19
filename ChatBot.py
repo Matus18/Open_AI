f188561decc02a245c456b994bc7ee5885df1aad
Matus18 <106945625+Matus18@users.noreply.github.com>
2022-12-19 10:42:48 -0300

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,23 +1,31 @@
+import os
 import openai
 
-openai.api_key = "API_KEY"
+openai.api_key = "API-KEY"
 
+start_sequence = "\nAI:"
+restart_sequence = "\nHuman: "
+
+prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: "
 conversation = ""
 
-i = 1
-while (i != 0):
-    question = input("Humano: ")
-    conversation += "\nHumano:" + question + "\nAI:"
-    response = openai.Completion.create(
-        engine = "davinci",
-        prompt = conversation,
-        temperature = 0.9,
-        max_tokens = 150,
-        top_p = 1,
-        frequency_penalty = 0,
-        presence_penalty = 0.6,
-        stop = ["\n", " Humano:", " AI:"]
-    )
-    answer = response.choices[0].text.strip()
-    conversation += answer
-    print("AI: " + answer + "\n")
+def openai_create(prompt):
+    i = 1
+    while(i != 0):
+        question = input("Humano: ")
+        conversation = "\nHumano:" + question + "\nAI:"
+        response = openai.Completion.create(
+            engine = "davinci",
+            prompt = prompt,
+            temperature = 0.9,
+            max_tokens = 150,
+            top_p = 1,
+            frequency_penalty = 0,
+            presence_penalty = 0.6,
+            stop = ["\n", " Humano:", " AI:"]
+        )
+        answer = response.choices[0].text.strip()
+        conversation += answer
+        print("AI: " + answer + "\n")
+
+openai_create(prompt)
